a6a65f286f336c7061832fec17f3fecc2045da94
Mohammad Alauddin Emon <95302046+Emon5419@users.noreply.github.com>
2022-11-16 23:32:23 +0600

    Picture-Animation

---
+++ src/Pages/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css'
+
+const Footer = () => {
+   return (
+      <div className='divF'>
+         <p>This is footer section</p>
+      </div>
+   );
+};
+
+export default Footer;
